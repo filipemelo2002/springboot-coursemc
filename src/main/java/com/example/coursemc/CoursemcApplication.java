e2af74b4cf1f8680f779701998521240400f645f
Filipe Melo <filipe.melo@liferay.com>
2021-12-23 21:49:39 -0300

    Add: Category instantiation operation

--- src/main/java/com/example/coursemc/CoursemcApplication.java
+++ src/main/java/com/example/coursemc/CoursemcApplication.java
@@ -1,13 +1,34 @@
 package com.example.coursemc;
 
+import com.example.coursemc.domain.Category;
+import com.example.coursemc.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CoursemcApplication {
+public class CoursemcApplication implements CommandLineRunner {
+
+
+    private final CategoryRepository categoryRepository;
+
+
+    public CoursemcApplication (CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoursemcApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
